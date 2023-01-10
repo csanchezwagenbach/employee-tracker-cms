cfb6880a8facc91f34ce455da091d61898b3acb6
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-09 21:25:53 -0800

    continued experimenting with sql queries, successfully created template for 'view all employees' query

--- index.js
+++ index.js
@@ -25,10 +25,17 @@ app.listen(PORT, () => {
 });
 
 db.query(`SELECT * FROM employee`, (req, res) => {
+    console.log("All employees")
     console.table(res)
 });
 
 db.query(`SELECT * FROM role`, (req, res) => {
+    console.log("All roles")
+    console.table(res)
+});
+
+db.query(`SELECT * FROM department`, (req, res) => {
+    console.log("All departments")
     console.table(res)
 });
 
@@ -42,16 +49,22 @@ ORDER BY role.id ASC`, (req, res) => {
     console.table(res)
 });
 
-db.query(`SELECT 
-* 
-FROM role
-JOIN department ON role.department_id = department.id`, (req, res) => {
+
+// DB QUERY FOR "VIEW ALL EMPLOYEES" 
+
+db.query(`SELECT
+e.id, e.first_name, e.last_name, title, name AS department, salary, concat(m.first_name, " ", m.last_name) AS manager
+FROM employee e
+INNER JOIN role ON e.role_id = role.id
+JOIN department ON role.department_id = department.id
+LEFT JOIN employee m ON m.id = e.manager_id 
+ORDER BY e.id ASC`, (req, res) => {
     console.table(res)
 });
 
-db.query(`SELECT * FROM department`, (req, res) => {
-    console.table(res)
-});
+
+
+
 
 
 
